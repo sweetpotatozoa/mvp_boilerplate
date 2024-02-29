d471a78458e9b68cca8b208beec45201b50e94a1
staend1 <ysyysy8656@gmail.com>
2024-02-29 16:12:38 +0900

    fix createTransaction의 데이터형식을 고침, createdAt: new Date()도 넣음

--- server/src/services/Buyer_Service.js
+++ server/src/services/Buyer_Service.js
@@ -1,4 +1,5 @@
 const TransactionRepo = require('../repositories/Transaction_Repo')
+const { ObjectId } = require('mongodb') // ObjectId를 위한 require 추가
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const configs = require('../utils/configs')
@@ -7,7 +8,7 @@ class BuyerService {
   async createTransaction(buyerId, brand, productCode, color, size) {
     const data = {
       buyerInfo: {
-        buyerId: buyerId,
+        buyerId: new ObjectId(buyerId), // buyerId를 ObjectId로 변환
         recipientName: null,
         recipientPhoneNumber: null,
         recipientAddress: null,
@@ -20,20 +21,25 @@ class BuyerService {
         size: size,
       },
       sellerInfo: {
-        sellerId: null,
+        sellerId: null, // sellerId도 필요한 경우 ObjectId로 변환
         sellerName: null,
         sellerPhoneNumber: null,
         returnAddress: null,
         accountNumber: null,
         accountHolderName: null,
       },
-
       transactionStatus: 0,
       isUniqueCodeRequest: false,
       uniqueCode: null,
+      createdAt: new Date(), // 현재 타임스탬프 생성
     }
+
+    // sellerId를 ObjectId로 저장해야 한다면, 해당 정보를 받아와서 new ObjectId(sellerId)로 변환해야 합니다.
+    // 예: data.sellerInfo.sellerId = new ObjectId(sellerId); // sellerId를 인자로 받는 경우
+
     const result = await TransactionRepo.createTransaction(data)
     return result
   }
 }
+
 module.exports = new BuyerService()
